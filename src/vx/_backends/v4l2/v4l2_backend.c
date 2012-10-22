92e27fca5abc49acfc02d87c1ea7b307b29fdf1f
Hartmut Seichter <hartmut@technotecture.com>
2012-10-22 23:39:18 +0200

    missing stuff

--- src/vx/_backends/v4l2/v4l2_backend.c
+++ src/vx/_backends/v4l2/v4l2_backend.c
@@ -1,14 +1,12 @@
 #include "_source.h"
 
 
-struct vx_source_v4l2_impl {
+typedef struct {
 	int fd;
-};
+} vx_source_v4l2_impl;
 
 
 int v4l2_source_open(vx_source* s, const char* n)
 {
 	vx_source_v4l2_impl* impl = (vx_source_v4l2_impl*)s->impl;
-
-
 }
